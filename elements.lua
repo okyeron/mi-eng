29416b97dfae04fd75ade5552466f27993d276d1
okyeron <1909661+okyeron@users.noreply.github.com>
2020-05-04 17:19:14 -0500

    add miElements lib

--- elements.lua
+++ elements.lua
@@ -1,26 +1,27 @@
 --
---         elements
+--    elements
 --
---    v 0.1.0 @okyeron
+--    v 0.2.0 @okyeron
+--
+--
+--
 --
 -- Based on the supercollider UGens by Volker Bohm https://github.com/v7b1/mi-UGens
 --
 --
 
+local UI = require "ui"
+local miElements = require "mi-eng/lib/mielements_engine"
+
 engine.name = "MiElements"
 
-    --blow_in=0, strike_in=0, gate=0, pit=48, strength=0.5, contour=0.2, bow_level=0,
-		--blow_level=0, strike_level=0, flow=0.5, mallet=0.5, bow_timb=0.5, blow_timb=0.5,
-		--strike_timb=0.5, geom=0.25, bright=0.5, damp=0.7, pos=0.2, space=0.3, model=0,
-		--mul=1.0, add=0;
-
 local gate = 0 -- (open for positive input values)
-local pit = 52 -- (midi note)
+local pit = 36 -- (midi note)
 local strength = 0.5 --(0 -- 1)
-local contour = 0.2
-local bow_level = 0.2
-local blow_level = 0.35
-local strike_level = 0.05
+local contour = 0.5
+local bow_level = 1
+local blow_level = 0
+local strike_level = 0
 local flow = 0.25
 local mallet = 0.5
 local bow_timb = 0.4
@@ -34,7 +35,9 @@ local space = 0.5
 local model = 0
 local mul = 1.0
 local add = 0
-
+local slide = {}
+local midiassignments = {"contour","bow_level","blow_level","strike_level","pit","strength","flow","mallet","geom","bright","bow_timb","blow_timb","strike_timb","damp","pos","space"}
+local midichannels = {32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47}
 
 local current_note = pit
 local mo = midi.connect() -- defaults to port 1 (which is set in SYSTEM > DEVICES)
@@ -47,12 +50,22 @@ mo.event = function(data)
     redraw()
   elseif d.type == "note_off" then
     engine.noteOff(0)
+  elseif d.type == "cc" then
+    --print ("cc: ".. d.cc .. " : " .. d.val)
+    for uis = 1, 16 do
+      if midichannels[uis] == d.cc then
+        slide[uis]:set_value (d.val/100)
+        params:set(midiassignments[uis], d.val/100)
+      end
+    end
+    redraw()
+
   end 
   
 end
 
 function init()
-  -- initialization
+  -- engine initialization
   engine.gate(gate)
   engine.pit(pit)
   engine.strength(strength)
@@ -73,64 +86,32 @@ function init()
   engine.model(model)
   engine.mul(mul)
 
+-- Add params
 
-  params:add_control("gate", "gate", controlspec.new(0, 1, "lin", 1, gate, ""))
-  params:set_action("gate", function(value) engine.gate(value) end)
-
-  params:add_control("pitch", "pitch", controlspec.new(0, 100, "lin", 1, pitch, ""))
-  params:set_action("pitch", function(value) engine.pit(value) end)
-
-  params:add_control("strength", "strength", controlspec.new(0.00, 1.00, "lin", 0.01, strength, ""))
-  params:set_action("strength", function(value) engine.strength(value) end)
-
-  params:add_control("contour", "contour", controlspec.new(0.00, 1.00, "lin", 0.01, contour, ""))
-  params:set_action("contour", function(value) engine.contour(value) end)
-
-  params:add_control("bow_level", "bow_level", controlspec.new(0.00, 1.00, "lin", 0.01, bow_level, ""))
-  params:set_action("bow_level", function(value) engine.bow_level(value) end)
-
-  params:add_control("blow_level", "blow_level", controlspec.new(0.00, 1.00, "lin", 0.01, blow_level, ""))
-  params:set_action("blow_level", function(value) engine.blow_level(value) end)
-
-  params:add_control("strike_level", "strike_level", controlspec.new(0.00, 1.00, "lin", 0.01, strike_level, ""))
-  params:set_action("strike_level", function(value) engine.strike_level(value) end)
-
-  params:add_control("flow", "flow", controlspec.new(0.00, 1.00, "lin", 0.01, flow, ""))
-  params:set_action("flow", function(value) engine.flow(value) end)
+  miElements.add_params()
  
-  params:add_control("mallet", "mallet", controlspec.new(0.00, 1.00, "lin", 0.01, mallet, ""))
-  params:set_action("mallet", function(value) engine.mallet(value) end)
 
-  params:add_control("bow_timb", "bow_timb", controlspec.new(0.00, 1.00, "lin", 0.01, bow_timb, ""))
-  params:set_action("bow_timb", function(value) engine.bow_timb(value) end)
+  -- UI
+  --for uis = 1, 16 do
+  --  slide[uis] = UI.Slider.new((uis*6)+20, 18, 4, 36, 0, 0, 1, {0.5})
+  --end
+  for uis = 1, 4 do
+    slide[uis] = UI.Dial.new(uis*16, 12, 10, 0, 0, 1, 0.01)
+  end 
+  for uis = 5, 6 do
+    slide[uis] = UI.Dial.new((uis*16)+16, 12, 10, 0, 0, 1, 0.01)
+  end 
+  for uis = 7, 10 do
+    slide[uis] = UI.Dial.new((uis-7)*20+40, 30, 10, 0, 0, 1, 0.01)
+  end 
+  for uis = 11, 16 do
+    slide[uis] = UI.Dial.new((uis-11)*16+30, 48, 10, 0, 0, 1, 0.01)
+  end 
 
-  params:add_control("blow_timb", "blow_timb", controlspec.new(0.00, 1.00, "lin", 0.01, blow_timb, ""))
-  params:set_action("blow_timb", function(value) engine.blow_timb(value) end)
-
-  params:add_control("strike_timb", "strike_timb", controlspec.new(0.00, 1.00, "lin", 0.01, strike_timb, ""))
-  params:set_action("strike_timb", function(value) engine.strike_timb(value) end)
-  
-  params:add_control("geom", "geom", controlspec.new(0.00, 1.00, "lin", 0.01, geom, ""))
-  params:set_action("geom", function(value) engine.geom(value) end)
-  
-  params:add_control("bright", "bright", controlspec.new(0.00, 1.00, "lin", 0.01, bright, ""))
-  params:set_action("bright", function(value) engine.bright(value) end)
-  
-  params:add_control("damp", "damp", controlspec.new(0.00, 1.00, "lin", 0.01, damp, ""))
-  params:set_action("damp", function(value) engine.damp(value) end)
-  
-  params:add_control("pos", "pos", controlspec.new(0.00, 1.00, "lin", 0.01, pos, ""))
-  params:set_action("pos", function(value) engine.pos(value) end)
-  
-  params:add_control("space", "space", controlspec.new(0.00, 1.00, "lin", 0.01, space, ""))
-  params:set_action("space", function(value) engine.space(value) end)
-  
-  params:add_control("model", "model", controlspec.new(0, 2, "lin", 1, model, ""))
-  params:set_action("model", function(value) engine.model(value) end)
-
-  params:add_control("mul", "mul", controlspec.new(0.00, 1.00, "lin", 0.01, mul, ""))
-  params:set_action("mul", function(value) engine.mul(value) end)
-  
+   for uis = 1, 16 do
+    slide[uis]:set_value (params:get(midiassignments[uis]))
+  end 
+ 
   redraw()
 end
 
@@ -139,6 +120,12 @@ function key(n,z)
     engine.noteOn(pit)
   else
     engine.noteOff(0)
+  end
+
+  if n == 3 and z == 1 then
+    engine.gate(1)
+  else
+    engine.gate(0)
   end
   
   -- key actions: n = number, z = state
@@ -166,42 +153,27 @@ end
 function redraw()
   -- screen redraw
   screen.clear()
-  screen.aa(0)
+  screen.aa(1)
   screen.level(15)
 
-  screen.move(8, 16)
-  screen.font_face(3)
-  screen.font_size(16)
-  screen.text("elements  ")
+  screen.move(0,0)
+  screen.stroke()
 
+  for uis = 1, 16 do
+    slide[uis]:redraw()
+  end
+
+  screen.move(8, 8)
   screen.font_face(1)
   screen.font_size(8)
-  screen.move(8, 32)
-  screen.text("note:  ")
-  screen.move(120, 32)
-  screen.text_right(current_note)
+  screen.text("elements  ")
 
-
-  screen.move(8, 42)
-  screen.text("bright:  ")
-  screen.move(120, 42)
-  screen.text_right(string.format("%.2f", params:get("bright")))
-
-  screen.move(8, 48)
-  screen.text("pos:  ")
-  screen.move(120, 48)
-  screen.text_right(string.format("%.2f", params:get("pos")))
-
-  screen.move(8, 54)
-  screen.text("space:  ")
-  screen.move(120, 54)
-  screen.text_right(string.format("%.2f", params:get("space")))
-
-  screen.move(8, 60)
-  screen.text("damp:  ")
-  screen.move(120, 60)
-  screen.text_right(string.format("%.2f", params:get("damp")))
-
+  --screen.font_face(1)
+  --screen.font_size(8)
+  --screen.move(90, 8)
+  --screen.text("note:  ")
+  --screen.move(120, 8)
+  --screen.text_right(current_note)
 
 
   screen.update()
