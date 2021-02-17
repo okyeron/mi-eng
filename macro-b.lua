f9f3ffe8bdd5f290a4b43d75d651d8d8774dcaaa
okyeron <1909661+okyeron@users.noreply.github.com>
2021-02-17 16:54:26 -0600

    remove pmap for 16n

--- macro-b.lua
+++ macro-b.lua
@@ -78,28 +78,28 @@ function init()
 
 
   -- create midi pmap for 16n
-  print ("check pmap")
-  local p = norns.pmap.data.contour
-  --p = pmap.get("contour")
-  if p == nil then
-    local i = defualt_midicc - 1
-    for k,v in ipairs(param_assign) do
-      controls[v].midi = i + 1 
-      norns.pmap.new(v)
-      norns.pmap.assign(v,1,1,controls[v].midi) -- (id, dev, ch, cc)
-      i = i + 1 
-    end
-    print ("created default pmap")
-    norns.pmap.write()
-  else 
-    --print ("already have pmap")
-    for k,v in pairs(norns.pmap.data) do
-      if controls[k] ~= nil then
-        controls[k].midi = v.cc
-      end
-    end
-    --tab.print (controls.bright)
-  end
+--  print ("check pmap")
+--  local p = norns.pmap.data.contour
+--  --p = pmap.get("contour")
+--  if p == nil then
+--    local i = defualt_midicc - 1
+--    for k,v in ipairs(param_assign) do
+--      controls[v].midi = i + 1 
+--      norns.pmap.new(v)
+--      norns.pmap.assign(v,1,1,controls[v].midi) -- (id, dev, ch, cc)
+--      i = i + 1 
+--    end
+--    print ("created default pmap")
+--    norns.pmap.write()
+--  else 
+--    --print ("already have pmap")
+--    for k,v in pairs(norns.pmap.data) do
+--      if controls[k] ~= nil then
+--        controls[k].midi = v.cc
+--      end
+--    end
+--    --tab.print (controls.bright)
+--  end
 
 -- MIDI
 local mo = midi.connect() -- defaults to port 1 (which is set in SYSTEM > DEVICES)
